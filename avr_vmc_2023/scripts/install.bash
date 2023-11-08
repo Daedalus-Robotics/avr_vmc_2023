d188c80c224880a7df8eb129a01e4d9bfd98eef0
literallyanything <hunterbaker@me.com>
2023-11-08 10:35:25 -0500

    Add fixed nvargus-daemon.service to install.bash

--- avr_vmc_2023/scripts/install.bash
+++ avr_vmc_2023/scripts/install.bash
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-sudo cp "$(dirname "$0")/vmc.service" /etc/systemd/system/
+sudo cp "$(dirname "$0")/*.service" /etc/systemd/system/
 
 sudo systemctl daemon-reload
+
+sudo systemctl enable nvargus-daemon.service
+sudo systemctl restart nvargus-daemon.service
+
 sudo systemctl enable vmc.service
-sudo systemctl start vmc.service
+sudo systemctl restart vmc.service
